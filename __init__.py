a0fab5884b4eea3fc0789e01110e2a4eb653cc32
vox <drewdebelo@gmail.com>
2024-07-17 20:47:24 +0100

    bboxtomask

--- __init__.py
+++ __init__.py
@@ -1,8 +1,10 @@
 from .ho_moondream import Moondream
+from .ho_bboxtomask import BboxToMask
 
 # A dictionary that contains all nodes you want to export with their names
 # NOTE: names should be globally unique
 
 NODE_CLASS_MAPPINGS = {
     "Moondream Interrogator": Moondream,
+    "BboxToMask": BboxToMask,
 }
